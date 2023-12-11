41c6628503c7240c0914c896e10774404a65d8ff
Alexey Korotin <sx12fx@gmail.com>
2023-12-11 18:08:53 +0300

    feat: build app

---
+++ assets/axios.4de53adf.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.587891a8.js";import{a as o}from"./axios.6e1fcf85.js";const t="http://localhost:3000",i=o.create({baseURL:t});var e=a(({app:s})=>{});export{i as api,e as default};
--- assets/axios.d8788516.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.afaf4dce.js";import{a as o}from"./axios.6e1fcf85.js";const t="http://localhost:3000",i=o.create({baseURL:t});var e=a(({app:s})=>{});export{i as api,e as default};
